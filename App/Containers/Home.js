0c6cec4b0ba26ec575c84b6d14ea3ef14bdcce61
Riken Patel <rik.patel2025@gmail.com>
2017-11-20 20:59:00 -0800

    initial  chat UI

--- App/Containers/Home.js
+++ App/Containers/Home.js
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Dimensions, TextInput, TouchableOpacity, Alert, Image } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+
+
 const window = Dimensions.get('window');
 
 export default class Login extends Component {
 
-    static navigationOptions = ({ navigation }) => {
-        return {
-            title: `Welcome ${navigation.state.params.screen}`,
-            headerLeft: null
-        }
-    };
-
-    render() {
-      const { state, navigate } = this.props.navigation;
-      return (
-        <View style={styles.container}>
-            
-        </View>
-      );
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: [],
     }
+  }
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://facebook.github.io/react/img/logo_og.png',
+          },
+        },
+      ],
+    });
+  }
+
+  static navigationOptions = ({ navigation }) => {
+      return {
+          title: `Welcome ${navigation.state.params.screen}`,
+          headerLeft: null
+      }
+  };
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  render() {
+    const { state, navigate } = this.props.navigation;
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    );
+  }
   }
   
   const styles = StyleSheet.create({
